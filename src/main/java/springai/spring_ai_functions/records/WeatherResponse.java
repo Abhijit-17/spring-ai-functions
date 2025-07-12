41bc562cbccd7d920168d02acf9be30f3868a0cb
Abhijit Bhadoria <abhijit.bhadoria@M-FVFG91RXQ05N.local>
2025-07-12 11:47:12 +0530

    weather response record/dto property binding

--- src/main/java/springai/spring_ai_functions/records/WeatherResponse.java
+++ src/main/java/springai/spring_ai_functions/records/WeatherResponse.java
@@ -4,30 +4,50 @@ import java.math.BigDecimal;
 
 import com.fasterxml.jackson.annotation.JsonClassDescription;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonClassDescription("WeatherResponse DTO")
 public record WeatherResponse(
+    @JsonProperty("cloud_pct")
     @JsonPropertyDescription("Percentage of cloud cover")
     Integer cloudPct,
+
+    @JsonProperty("temp")
     @JsonPropertyDescription("Temperature in degrees Celsius")
     Integer temp,
+
+    @JsonProperty("feels_like")
     @JsonPropertyDescription("\"Feels like\" temperature in Celsius, considers humidity and wind")
     Integer feelsLike,
+
+    @JsonProperty("humidity")
     @JsonPropertyDescription("Relative humidity percentage (0-100%)")
     Integer humidity,
+
+    @JsonProperty("min_temp")
     @JsonPropertyDescription("Minimum temperature in Celsius for the location")
     Integer minTemp,
+
+    @JsonProperty("max_temp")
     @JsonPropertyDescription("Maximum temperature in Celsius for the location")
     Integer maxTemp,
+
+    @JsonProperty("wind_speed")
     @JsonPropertyDescription("Wind speed in km/h")
     BigDecimal windSpeed,
+
+    @JsonProperty("wind_degrees")
     @JsonPropertyDescription("Wind direction in degrees (0-360, where 0 is North, 90 is East, etc.)")
     Integer windDegrees,
-    @JsonPropertyDescription("Epoch timestamp of Sunrise in seconds")
+
+    @JsonProperty("sunrise")
+    @JsonPropertyDescription("Epoch timestamp of Sunrise in seconds GMT")
     Long sunrise,
-    @JsonPropertyDescription("Epoch timestamp of Sunset in seconds")
+
+    @JsonProperty("sunset")
+    @JsonPropertyDescription("Epoch timestamp of Sunset in seconds GMT")
     Long sunset
 ) {
 
