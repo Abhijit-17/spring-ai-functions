55324b793a6f1100d7de929f159732eaeaa2010d
Abhijit Bhadoria <abhijit.bhadoria@M-FVFG91RXQ05N.local>
2025-07-30 15:45:37 +0530

    code cleanup

--- src/main/java/springai/spring_ai_functions/services/OpenAIService.java
+++ src/main/java/springai/spring_ai_functions/services/OpenAIService.java
@@ -2,8 +2,6 @@ package springai.spring_ai_functions.services;
 
 import springai.spring_ai_functions.records.Answer;
 import springai.spring_ai_functions.records.Question;
-import springai.spring_ai_functions.records.WeatherRequest;
-import springai.spring_ai_functions.records.WeatherResponse;
 
 public interface OpenAIService {
 
